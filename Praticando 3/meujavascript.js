5173f3fe48dde2ed4f1e5abff88babaf4757c6ac
MarcosAra <87018314+MarcosAra@users.noreply.github.com>
2022-05-31 10:59:54 -0300

    Add files via upload

---
+++ Praticando 3/meujavascript.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+
+
+
+/*FUNÇÃO PARA TROCAR O TEXTO*/
+function trocatexto(){
+				document.getElementById("demo").innerHTML="VAI VOLTARRRRR!!!!!!!";
+		
+	}
+
+function trocar(){
+			document.getElementById('myImagem').src="img/hunter2.jpg";
+		}
+		function voltar(){
+			document.getElementById('myImagem').src='img/hunter3.jpg';
+		}
+
+
+function alterar(){
+			document.getElementById('demo').style.fontSize='35px';
+		}
+function resetar(){
+			document.getElementById('demo').style.fontSize='16px';
+		}
+
+
+function ocultar(){
+			document.getElementById('demo').style.display='none'
+		}
+		function exibir(){
+			document.getElementById('demo').style.display='block';
+		}
